772b520a0e3c45445a7142f4a5e492eb04778bce
Muhammad Zohaib Shabbir <MCF2000656@ue.edu.pk>
2021-06-07 08:22:20 +0500

    anim

--- app/src/main/java/com/slifix/slifix/MainActivity.java
+++ app/src/main/java/com/slifix/slifix/MainActivity.java
@@ -1,5 +1,6 @@
 package com.slifix.slifix;
 
+import android.app.ActivityOptions;
 import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
@@ -8,19 +9,28 @@ import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.os.Handler;
 import android.provider.Settings;
+import android.util.Pair;
 import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.slifix.slifix.login.LoginScreen;
 
 public class MainActivity extends AppCompatActivity {
+    Animation animation;
+    ImageView splash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        animation = AnimationUtils.loadAnimation (this,R.anim.splash);
+        splash = findViewById (R.id.splashImage);
+        splash.setAnimation (animation);
         if (DataManager.getAuthToken () == null){
             startService(new Intent(this, DataManager.class));
         }
@@ -29,22 +39,17 @@ public class MainActivity extends AppCompatActivity {
         if (status == "Internet Access"){
             new Handler().postDelayed(() -> {
                 Intent intent = new Intent(MainActivity.this, LoginScreen.class);
-//                Intent intent = new Intent(MainActivity.this, Checkout.class);
-                startActivity(intent);
-                finish();
+                Pair pair = new Pair (splash,"logoImage");
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation (this,pair);
+                    startActivity(intent,options.toBundle ());
+                }
+                finish ();
             },2000);
 
         }else{
             startActivity(new Intent (Settings.ACTION_WIFI_SETTINGS));
             Toast.makeText(getApplicationContext (), status, Toast.LENGTH_LONG).show();
         }
-
-//        LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE );
-//        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
-//            buildAlertMessageNoGps();
-//        }else{
-//        MyReceiver = new MyReceiver();
-//        registerReceiver(MyReceiver, new IntentFilter (ConnectivityManager.CONNECTIVITY_ACTION));
-//        }
     }
 }
